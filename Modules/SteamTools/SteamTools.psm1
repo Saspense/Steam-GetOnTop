9af53755cb5d19d883317ec0878e56f23843a86d
ApocalyptoSoldier <54095452+ApocalyptoSoldier@users.noreply.github.com>
2022-08-17 11:08:09 +0200

    Update SteamTools.psm1
    Added Get-LibraryFolders

--- Modules/SteamTools/SteamTools.psm1
+++ Modules/SteamTools/SteamTools.psm1
@@ -159,3 +159,38 @@ param(
 
 	return "7656119$(($Z * 2) + (7960265728 + $Y))"
 }
+Function Get-LibraryFolders()
+{
+<#
+.Synopsis 
+	Retrieves library folder paths from .\SteamApps\libraryfolders.vdf
+.Description
+	Reads .\SteamApps\libraryfolders.vdf to find the paths of all the library folders set up in steam
+.Example 
+	$libraryFolders = Get-LibraryFolders
+	Description 
+	----------- 
+	Retrieves a list of the library folders set up in steam
+#>
+	$steamPath = Get-SteamPath
+	
+	$vdfPath = "$($steamPath)\SteamApps\libraryfolders.vdf"
+	
+	[array]$libraryFolderPaths = @()
+	
+	if (Test-Path $vdfPath)
+	{
+		$libraryFolders = ConvertFrom-VDF (Get-Content $vdfPath -Encoding UTF8) | Select -ExpandProperty libraryfolders
+		
+		$libraryFolderIds = $libraryFolders | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name
+		
+		ForEach ($libraryId in $libraryFolderIds)
+		{
+			$libraryFolder = $libraryFolders.($libraryId)
+			
+			$libraryFolderPaths += $libraryFolder.path.Replace('\\','\')
+		}
+	}
+	
+	return $libraryFolderPaths
+}
